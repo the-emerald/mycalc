bc4a39906a686d4fdd24e4e5d724ebfeb7b2bb17
Anson <git@anson-cheung.com>
2019-09-25 20:24:15 +0100

    Convert epic test to grid

--- epic_test.py
+++ epic_test.py
@@ -1,31 +1,30 @@
 import sys
-
 from PySide2.QtCore import Qt
 from PySide2.QtWidgets import *
 
 
 class Form(QDialog):
     def __init__(self, parent=None):
+        super().__init__(parent)
         self.epic_level = 0
 
-        super(Form, self).__init__(parent)
-        self.setWindowTitle("Epic")
+        self.setWindowTitle("Grid")
+        grid = QGridLayout()
+        self.setLayout(grid)
+
         self.label = QLabel("Epic level:")
         self.output = QLineEdit(f"{self.epic_level}")
         self.output.setFocusPolicy(Qt.NoFocus)
 
         self.epic = QPushButton("Epic")
         self.notepic = QPushButton("Not epic")
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        layout.addWidget(self.output)
-        layout.addWidget(self.epic)
-        layout.addWidget(self.notepic)
-
-        self.setLayout(layout)
         self.epic.clicked.connect(self.more_epic)
         self.notepic.clicked.connect(self.less_epic)
+
+        grid.addWidget(self.label, 0, 0, 1, 2)
+        grid.addWidget(self.output, 1, 0, 1, 2)
+        grid.addWidget(self.epic, 2, 1, 1, 1)
+        grid.addWidget(self.notepic, 2, 0, 1, 1)
 
     def update_output(self):
         self.output.text()
